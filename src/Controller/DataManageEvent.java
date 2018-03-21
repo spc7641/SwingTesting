3c2c39ca7d092b0a8c679c5f9130fe7cafc902d6
spc7641 <spc7641@g.rit.edu>
2018-03-21 17:51:34 -0400

    Improvement to the table, to dow display headers, with some sample dummy data put it.

--- src/Controller/DataManageEvent.java
+++ src/Controller/DataManageEvent.java
@@ -6,28 +6,59 @@ import java.awt.event.ActionListener;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.WindowConstants;
+import javax.swing.table.AbstractTableModel;
 
 /**
  * Created by Shawn on 3/21/2018.
  */
 public class DataManageEvent implements ActionListener {
 
+  private JFrame dataFrame;
+
+  private JTable table;
+
+  private String[] columnNames = {"Number","Letter"};
+
+  private Object[][] data = {{1,"a"},{2,"b"},{3,"c"},{4,"d"}};
   @Override
   public void actionPerformed(ActionEvent e) {
-    JFrame data = new JFrame("Data Manager");
+    dataFrame = new JFrame("Data Manager");
 
-    JTable table = new JTable(3,3);
+    JTable table = new JTable(new AbstractTableModel() {
+      @Override
+      public String getColumnName(int col){
+        return columnNames[col];
+      }
+      @Override
+      public int getRowCount() {
+        return data.length;
+      }
 
-    Box layout = new Box(BoxLayout.Y_AXIS);
+      @Override
+      public int getColumnCount() {
+        return columnNames.length;
+      }
 
-    layout.add(table);
+      @Override
+      public Object getValueAt(int rowIndex, int columnIndex) {
+        return data[rowIndex][columnIndex];
+      }
 
-    data.add(layout);
-    data.pack();
-    data.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-    data.setVisible(true);
+      @Override
+      public boolean isCellEditable(int row, int col){
+        return false;
+      }
+    });
+
+    JScrollPane layout = new JScrollPane(table);
+
+    dataFrame.add(layout);
+    dataFrame.pack();
+    dataFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    dataFrame.setVisible(true);
     Main.setFocus();
   }
 }
